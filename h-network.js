10c432856e7c128ca4bf6bff2e2200dda6f84755
徐志刚 <xuzg@ziroom.com>
2019-08-22 00:18:12 +0800

    [nodestudy] 学习编写 node 命令行工具

--- h-network.js
+++ h-network.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 1234;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello, world");
+  res.end("Hello, webpack");
 });
 
 server.listen(port, () => {
